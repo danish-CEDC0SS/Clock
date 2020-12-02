74beec4b74edcdfe28f98ef1e71b8d45222c778f
danish-lucifer <74283994+danish-lucifer@users.noreply.github.com>
2020-12-02 10:49:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-// a=180;
 function fun1(){
-    // a=a+6;
     var now=new Date();
     var s=now.getSeconds();
     var m=now.getMinutes();
-    // var h=now.getHours();
+    var h=now.getHours();
     var s1=s*6+180;
     var m1=m*6+180;
-    console.log(s,m,s1,m1,'asdad');
-    // var h1=h*
+    var i=m/60;
+    if(h>12){
+        h=h-12;
+    }
+    var h1=(h+i)*30+180;
+    console.log(h1,'ewr');
+    console.log(s,m,h,s1,m1,h1,i,'asdad');
     var x=document.getElementById("sec");
     var y=document.getElementById("minute");
-    // var x=document.getElementById("sec");
+    var z=document.getElementById("hour");
     x.style.transform="rotate("+s1+"deg)";
     y.style.transform="rotate("+m1+"deg)";
+    z.style.transform="rotate("+h1+"deg)";
 }
 setInterval(fun1,1000)
